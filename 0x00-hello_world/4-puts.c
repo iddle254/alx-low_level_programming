752f436a4bd3d21a5e928d5a53927264fa3ce0dc
iddle254 <charlesiddle99@gmail.com>
2022-09-09 04:56:07 -0700

    puts printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * 
+ *
+ *    * Description: 'Prints text using puts'
+ *
+ *     * 
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
 int main(void)
 {
-	/**
-	 * Puts out text and returns 0
-	 */
 	char puzzle[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(puzzle);
