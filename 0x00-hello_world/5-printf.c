88c44847c28a9760ce9da232178ed1821fcbb75c
iddle254 <charlesiddle99@gmail.com>
2022-09-09 05:06:21 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Prints text using printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char puzzle[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s",puzzle);
+	return (0);
+}
