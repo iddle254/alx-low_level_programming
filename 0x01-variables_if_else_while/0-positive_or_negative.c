3f435193ac431db35c77fce499039e3a4f1124f3
iddle254 <charlesiddle99@gmail.com>
2022-09-09 19:18:44 -0700

    pos or neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /** 
  * main - Entry point
  * Description: 'Prints text using printf'
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
