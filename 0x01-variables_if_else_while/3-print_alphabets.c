2d2f5f5146b45851d0bca5c4456aa19a2ce517d8
iddle254 <charlesiddle99@gmail.com>
2022-09-10 04:58:19 -0700

    upper and lower case alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,15 +11,15 @@
 int main(void)
 {
 	int i;
-	char alphabet[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
+	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
 		, 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	char upper_case;
 
-	for (i = 0; i < 27; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alphabet[i]);
 	}
-	for (i=0; i < 27; i++)
+	for (i=0; i < 26; i++)
 	{
 		upper_case = toupper((unsigned char) alphabet[i]);
 		putchar(upper_case);
