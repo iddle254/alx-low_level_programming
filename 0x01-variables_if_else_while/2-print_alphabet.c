3471254981805b13591be18033a2c3289a141956
iddle254 <charlesiddle99@gmail.com>
2022-09-09 22:00:12 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ * Description: 'Prints text using printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alphabet[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
+		, 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+	for (i = 0; i < 27; i++)
+		putchar(alphabet[i]);
+	putchar('\n');
+
+	return (0);
+}
