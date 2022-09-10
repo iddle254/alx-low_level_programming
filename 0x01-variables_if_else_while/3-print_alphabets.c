e63fb13739129e66188161c645e5974a72cc7c00
iddle254 <charlesiddle99@gmail.com>
2022-09-10 04:46:12 -0700

    alphabets in upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ * Description: 'Prints text using printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alphabet[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
+		, 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char upper_case;
+
+	for (i = 0; i < 27; i++)
+	{
+		upper_case = toupper((unsigned char) alphabet[i]);
+		putchar(alphabet[i]);
+		putchar(upper_case);
+	}
+	return (0);
+}
