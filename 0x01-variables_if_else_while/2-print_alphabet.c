0991183365c6fd9be72eb83bc27b6736ff4c85e8
iddle254 <charlesiddle99@gmail.com>
2022-09-11 00:22:27 -0700

    insomnia game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,7 +10,8 @@
 int main(void)
 {
 	int i;
-	char alphabet[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
+	char alphabet[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
 		, 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 	for (i = 0; i < 27; i++)
