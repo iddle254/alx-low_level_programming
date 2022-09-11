94ae841dcbaafe90254826dc4f5aecd26fa3fb8b
iddle254 <charlesiddle99@gmail.com>
2022-09-11 01:56:28 -0700

    0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ * Description: 'Prints text using printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
