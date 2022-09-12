5f0a0c3d9b9c569b5e5e3bf291aaebe2eccf921c
iddle254 <charlesiddle99@gmail.com>
2022-09-12 15:25:10 -0700

    possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ * Description: 'Prints text using printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 9)
+		{
+			break;
+		}
+		for (j = 1; j < 10; j++)
+		{
+			if (i == j)
+				continue;
+			else
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+			}
+		
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
