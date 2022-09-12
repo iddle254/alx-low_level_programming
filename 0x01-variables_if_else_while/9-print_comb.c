c39a61049bed5421273a3b983c8440e04783d9fc
iddle254 <charlesiddle99@gmail.com>
2022-09-11 22:51:01 -0700

    0-9 spaced

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ * Description: 'Prints text using printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
