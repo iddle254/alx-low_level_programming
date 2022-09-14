2de3402dd52c8087b8875603eea9e43fc88db37e
iddle254 <charlesiddle99@gmail.com>
2022-09-14 14:32:45 -0700

    multiple files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * Main description goes here
- **/
+ * Entry point - Main
+ * Description: prints _putchar
+ * Return: 0
+ */
 int main(void)
 {
 	char to_print[20] = "_putchar";
 
 	_putchar(to_print);
 
-	return (0);
-	
+	return (0);	
 }
