03f34b2fd0f0b4f9631dd85e532d206108f7ca64
iddle254 <charlesiddle99@gmail.com>
2022-09-14 14:45:24 -0700

    multiple files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * Main description goes here
+ **/
 int _putchar(char print_this[]);
+
+int _putchar(char print_this[])
+{
+	int i, c;
+
+	for (i = 0; (c = getchar()) != '\n', i++)
+	{
+		putchar(print_this[i]);
+	}
+	return (0);
+	
+}
