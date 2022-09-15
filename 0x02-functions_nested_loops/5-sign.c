f1530eec7a5635df67c92fd8bf6444129af4aff4
iddle254 <charlesiddle99@gmail.com>
2022-09-15 10:03:38 -0700

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,19 @@
  */
 int print_sign(int n)
 {
-	switch (n)
+	if (n > 0)
 	{
-		case n > 0:
-			_putchar('+');
-			return (1);
-			break;
-		case n == 0:
-			_putchar(0);
-			return (0);
-			break;
-		default:
-			_putchar('-');
-			return (-1);
-			break;
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return(-1);
 	}
 }
