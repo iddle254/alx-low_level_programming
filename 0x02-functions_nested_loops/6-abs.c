258fb4c0da7f7da665fff29c4ecc88a9ff9bfafe
iddle254 <charlesiddle99@gmail.com>
2022-09-15 11:49:57 -0700

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: number to calculate
+ * Description: computes the absolute value of an integer
+ * Return: absolutevalue
+ */
+int _abs(int n)
+{
+	return abs(n);
+}
