41a2e453bd08556297883d82c904df3c8e9b8519
iddle254 <charlesiddle99@gmail.com>
2022-09-15 00:17:07 -0700

    is lower>

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,11 +6,11 @@
  * Description: checks for lowercase letters
  * Return: 1 if lowercase 0 otherwise
  */
-int _islower(int c)
+int _islower(int c)/*params: c - letter to check*/
 {
 	if (islower(c))
 	{
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
