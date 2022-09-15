446b071797fb39a974c8b34f47ed0a19cba6b226
iddle254 <charlesiddle99@gmail.com>
2022-09-15 16:47:54 -0700

    multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * multiples - Entry point
+ * Description: adds all multiples of 3 or 5 under 1024
+ * Return: null
+ */
+void multiples(void)
+{
+	int curr_num = 3;
+	int total_sum;
+
+	while (curr_num < 1024)
+	{
+		if ((curr_num % 3 == 0) || (curr_num % 5 == 0))
+		{
+			total_sum += curr_num;
+		}
+		++curr_num;
+	}
+	printf("%d\n", total_sum);
+}
