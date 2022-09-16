3d09747a83c930fdd3f52ee245a53c575be3d7cb
iddle254 <charlesiddle99@gmail.com>
2022-09-16 06:03:29 -0700

    0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0-9
+ * Description: print from 0-9
+ * Return: null
+ */
+void print_numbers(void)
+{
+	int count;
+
+	count = 0;
+	while (count < 10)
+	{
+		_putchar(count);
+		count++;
+	}
+}
