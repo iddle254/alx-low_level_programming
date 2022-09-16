7b8321ed72a1446e5afa61c36be1ee20c51f75ca
iddle254 <charlesiddle99@gmail.com>
2022-09-16 08:17:07 -0700

    0-14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
  * more_numbers - print numbers from 0-14
- * Description: a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * Description: a function that prints 10 times the
+ * numbers, from 0 to 14, followed by a new line.
  * Return: null
  */
 void more_numbers(void)
