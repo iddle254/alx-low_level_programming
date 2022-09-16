de9e6b0badfc24ed49b7a26871abc7cdc1cfd7a9
iddle254 <charlesiddle99@gmail.com>
2022-09-16 13:41:41 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * fizz_buzz - interview question
+ * Description: Write a program that prints the numbers from 1 to 100,
+ * followed by a new line. But for multiples of three print Fizz 
+ * instead of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ * Return: void*/
+void fizz_buzz(void)
+{
+	int pointer;
+
+	for (pointer = 1; pointer <= 100; pointer++)
+	{
+		if (((pointer % 3) == 0) && ((pointer % 5) == 0))
+		{
+			printf("FizzBuzz");
+		} else if ((pointer % 3) == 0)
+		{
+			printf("Fizz");
+		} else if ((pointer % 5) == 0)
+		{
+			printf("Buzz");
+		} else
+		{
+			printf("%d", pointer);
+		}
+		if (pointer != 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
+/**
+ * main - entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
