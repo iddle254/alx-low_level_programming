1a85c9ecb920c910149b8ea42ce1102d7b46960e
iddle254 <charlesiddle99@gmail.com>
2022-09-16 08:15:45 -0700

    0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - print numbers from 0-14
+ * Description: a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * Return: null
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,5 @@ int mul(int a, int b);
 void print_numbers(void);
 void _putchar(int c);
 void print_most_numbers(void);
+void more_numbers(void);
 #endif
