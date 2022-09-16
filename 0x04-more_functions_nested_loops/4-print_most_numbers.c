e16ddb54fd70f7f8326995740f16d3cd6620e86b
iddle254 <charlesiddle99@gmail.com>
2022-09-16 07:31:10 -0700

    dont print 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,12 @@ void print_most_numbers(void)
 	count = 0;
 	while (count < 10)
 	{
-		if (count != 2 || count != 4)
+		if ((count == 2) || (count == 4))
 		{
-			_putchar(count + '0');
-			++count;
+			continue;
 		}
+		_putchar(count + '0');
+		count++;
 	}
 	_putchar('\n');
 }
