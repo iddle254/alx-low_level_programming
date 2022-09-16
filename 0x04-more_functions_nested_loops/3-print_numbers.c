28f0d2613579d6ab04eaed2b67c4e02cac1205c2
iddle254 <charlesiddle99@gmail.com>
2022-09-16 06:15:32 -0700

    0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -14,4 +14,5 @@ void print_numbers(void)
 		_putchar(count);
 		count++;
 	}
+	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _putchar - prints a single character
+ * @c: character to print
+ * Description: Takes a character and prints it
+ * Return: null
+ */
+void _putchar(int c)
+{
+	putchar(c + '0');
+}
