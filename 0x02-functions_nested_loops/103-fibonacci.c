64fc4ca2d881c16087958ad591c7372e7336d0d7
iddle254 <charlesiddle99@gmail.com>
2022-09-15 17:43:24 -0700

    sum of even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * fibonacci_even - find even fibonacci
+ * Description: gets the sum of even fibonacci numbers
+ * Return: null
+ */
+void fibonacci_even(void)
+{
+	int count = 3;
+	long int first = 1, second = 2, next = first + second, even_sum;
+
+	if (next <= 4000000)
+	{
+		((first % 2) == 0) ? even_sum += first : ;;
+		((second % 2) == 0) ? even_sum += second : ;;
+		first = second;
+		second = next;
+		next = first + second;
+		count++;
+	}
+	printf("%lu, ", even_sum);
+}
+/**
+ * main - Entry point
+ * Description: calls muliples function
+ * Return: null
+ */
+int main(void)
+{
+	fibonacci_even();
+	return (0);
+}
