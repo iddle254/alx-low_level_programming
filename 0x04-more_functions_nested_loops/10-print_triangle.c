2437f1ebc7b81195ca09ac317d59cfa8617da7ca
iddle254 <charlesiddle99@gmail.com>
2022-09-16 14:03:01 -0700

    draw triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,9 +11,9 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-	for (i = 1; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j < size; j++)
+		for (j = 1; j <= size; j++)
 		{
 			if ((i + j) <= size)
 				_putchar(' ');
