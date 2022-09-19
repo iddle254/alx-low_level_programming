6b2062f55de99895e07d48b5da10fa564fd1c9d1
iddle254 <charlesiddle99@gmail.com>
2022-09-19 16:26:07 -0700

    swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer param
+ * @b: pointer param
+ * Description: swaps the values of two integers
+ * Return: null
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = *c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 /* Prototypes for the functions */
 void reset_to_98(int *n);
 void _putchar(char c);
+void swap_int(int *a, int *b);
 #endif
