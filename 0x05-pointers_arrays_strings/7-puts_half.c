b1f26db8da346427646937a99e15d4cdbec23c2e
iddle254 <charlesiddle99@gmail.com>
2022-09-19 19:43:11 -0700

    print second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,8 +2,7 @@
 /**
  * puts_half - function that prints half of a string, followed by a new line
  * @str: pointer param
- * Description: 
- * n that prints half of a string, followed by a new line
+ * Description: functionn that prints half of a string, followed by a new line
  * Return: null
  */
 void puts_half(char *str)
@@ -18,12 +17,11 @@ void puts_half(char *str)
 		{
 			_putchar(str[i]);
 		}
-	
-	_putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-		for (i = (length - 1) / 2; str[i] != '\0'; ++i)
+		for (i = (length - 2) / 2; str[i] != '\0'; ++i)
 		{
 			_putchar(str[i]);
 		}
