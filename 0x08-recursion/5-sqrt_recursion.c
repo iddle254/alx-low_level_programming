44e898e94df946e60520471824826bd8cb11fed2
charles <charlesiddle99@gmail.com>
2022-09-28 16:30:15 +0300

    Sqrt recursion (#38)
    * implemented the sqrt_recursion function
    
    * implemented the sqrt_recursion function
    
    * fixed broken test case

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,30 @@
+#include "main.h"
+
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to be used
  *
- * @n : input number
- * @k: minimum number to guess
- *
- * Return: square root of @n or -1
-*/
-int isqrt_rec(int k, int n) {
-	int next_k = (k + n/k) / 2;
-	if (k*k <= n && (k+1)*(k+1) > n)
-		return k;
-	else
-		return isqrt_rec(next_k, n);
+ * Return: the square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 1 || n == 0)
+		return (n);
+	return (_sqrt(0, n));
 }
 
 /**
- * _sqrt_recursion - a function that returns the natural
- *                   square root of a number
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @x: squared number
  *
- * @n: input number
- *
- * Return: square root
-*/
-int _sqrt_recursion(int n)
+ * Return: the square root of n
+ */
+int _sqrt(int n, int x)
 {
-	return isqrt_rec(1, n);
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
