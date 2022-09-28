213d85e062c12cfae49a1ff2638d725d202b71ad
Moss <charlesiddle99@gmail.com>
2022-09-28 16:19:54 +0300

    implemented the sqrt_recursion function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,27 +2,16 @@
  * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
  *
  * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
+ * @k: minimum number to guess
  *
  * Return: square root of @n or -1
 */
-int _sqrt_recursion_wrapper(int n, int min, int max)
-{
-	int guess, guess_squared;
-
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
-
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
-		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
-}
+int isqrt_rec(int k, int n) {
+	int next_k = (k + n/k) / 2;
+	if (k*k <= n && (k+1)*(k+1) > n)
+		return k;
+	else
+		return isqrt_rec(next_k, n);
 
 /**
  * _sqrt_recursion - a function that returns the natural
@@ -34,12 +23,5 @@ int _sqrt_recursion_wrapper(int n, int min, int max)
 */
 int _sqrt_recursion(int n)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
-		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
-		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
-		return (-1);
-	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+	return isqrt_rec(1, n);
 }
