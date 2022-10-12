123cd00be84de29155fe4d25cf87eb095156b708
Moss <charlesiddle99@gmail.com>
2022-10-12 03:56:21 +0300

    fixed pointer issue

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,10 +14,10 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: ");
-		d.name == NULL ? printf("(nil)\n") : printf("%s\n", d.name);
+		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
 		printf("Age: ");
-		d.age == NULL ? printf("(nil)\n") : printf("%f\n", d.age);
+		d->age == NULL ? printf("(nil)\n") : printf("%f\n", d->age);
 		printf("Owner: ");
-		d.owner == NULL ? printf("(nil)\n") : printf("%s\n", d.owner);
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
 }
