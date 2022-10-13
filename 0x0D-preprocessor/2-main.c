dc105792f73959e667ab83f553e6a67a175bde5f
Moss <charlesiddle99@gmail.com>
2022-10-13 04:54:52 +0300

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints file name followed by a new line
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
