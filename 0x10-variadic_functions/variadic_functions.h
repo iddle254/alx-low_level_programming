749317ef0ffcb80b73c45a4dea8e3d93b5c94d09
Moss <charlesiddle99@gmail.com>
2022-10-16 04:30:48 +0300

    added missing headers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,9 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+#include <stdio.h>
+#include <stdarg.h>
+
 /**
  * struct print - A new struct type defining our print
  *
