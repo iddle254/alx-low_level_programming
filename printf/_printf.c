2d04c93aa068760ad715111e3008d7105a48db80
Moss <charlesiddle99@gmail.com>
2022-10-20 11:06:52 +0300

    added the putchar method

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,6 +1,19 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 /**
  * _printf - a function that produces output according to a format.
  *
